05913363cb465103cf0378b35ff43942ba7189d3
Documenter.jl <documenter@juliadocs.github.io>
2023-03-13 19:38:30 +0000

    build based on ebca443

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaGendUniv_Types","category":"page"},{"location":"#JuliaGendUniv_Types","page":"Home","title":"JuliaGendUniv_Types","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaGendUniv_Types.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaGendUniv_Types]","category":"page"}]
+}
