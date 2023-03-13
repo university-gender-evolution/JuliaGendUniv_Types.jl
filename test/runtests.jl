dc44936f33cb7cb4b122109c579f9d02475204aa
Krishna Bhogaonker <cyclotomiq@gmail.com>
2023-03-13 14:42:09 -0400

    updated test cases to match template.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
 using JuliaGendUniv_Types
 using Test
+using TestItems
 
-@testset "JuliaGendUniv_Types.jl" begin
-    # Write your tests here.
+
+@testitem "[JuliaGendUniv_Types] environment setup" begin
+
+    using JuliaGendUniv_Types, Test
+
+    cd(@__DIR__)
+    @show pwd()
+    
+    @test UM() isa JuliaGendUniv_Types.AbstractGendUnivDataConfiguration 
+    @test DataAudit() isa JuliaGendUniv_Types.AbstractDataChecks
+    @test NoAudit() isa JuliaGendUniv_Types.AbstractDataChecks
+    
+end
+
+
+@testitem "[JuliaGendUniv_Types] optimization module" begin
+    using JuliaGendUniv_Types, Test
+
+    @test 1 + 1 == 2
+
 end
